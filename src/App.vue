89c7098ae6f5253c841b1e10ec8a28ee93d07562
WeCr8 <zach@wecr8.info>
2025-05-03 17:43:50 -0700

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -4,15 +4,15 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <header>
+  <header class="app-header">
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
 
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
+      <nav class="navigation">
+        <RouterLink to="/" exact-active-class="active">Home</RouterLink>
+        <RouterLink to="/about" exact-active-class="active">About</RouterLink>
       </nav>
     </div>
   </header>
@@ -21,65 +21,67 @@ import HelloWorld from './components/HelloWorld.vue'
 </template>
 
 <style scoped>
-header {
+.app-header {
   line-height: 1.5;
   max-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 }
 
 .logo {
-  display: block;
   margin: 0 auto 2rem;
 }
 
-nav {
-  width: 100%;
-  font-size: 12px;
+.wrapper {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.navigation {
+  font-size: 14px;
+  margin-top: 1rem;
   text-align: center;
-  margin-top: 2rem;
 }
 
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
+.navigation a {
   display: inline-block;
   padding: 0 1rem;
   border-left: 1px solid var(--color-border);
+  color: inherit;
+  text-decoration: none;
 }
 
-nav a:first-of-type {
-  border: 0;
+.navigation a:first-of-type {
+  border-left: none;
+}
+
+.navigation a.active {
+  color: var(--color-text);
+  font-weight: bold;
 }
 
 @media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+  .app-header {
+    flex-direction: row;
+    justify-content: space-between;
+    padding: 1rem 2rem;
   }
 
   .logo {
     margin: 0 2rem 0 0;
   }
 
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
+  .wrapper {
+    flex-direction: row;
+    align-items: center;
   }
 
-  nav {
-    text-align: left;
-    margin-left: -1rem;
+  .navigation {
+    margin-top: 0;
     font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
+    margin-left: 2rem;
   }
 }
 </style>
