6b565f801267d18332cfd83da8c000094898b64c
WeCr8 <zach@wecr8.info>
2025-05-12 12:43:46 +0000

    remove @utils for rebuild

--- vite.config.js
+++ vite.config.js
@@ -16,15 +16,15 @@ export default defineConfig(({ mode }) => {
       vueJsx(), // 🧩 Enable JSX/TSX support in Vue components
     ],
     resolve: {
-  alias: {
-    '@': fileURLToPath(new URL('./src', import.meta.url)),
-    '@assets': path.resolve(__dirname, './src/assets'),
-    '@components': path.resolve(__dirname, './src/components'),
-    '@views': path.resolve(__dirname, './src/views'),
-    '@composables': path.resolve(__dirname, './src/composables'),
-    '@utils': path.resolve(__dirname, './src/composables/utils'), // ✅ FIXED
-  },
-},
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+        '@assets': path.resolve(__dirname, './src/assets'),
+        '@components': path.resolve(__dirname, './src/components'),
+        '@views': path.resolve(__dirname, './src/views'),
+        '@composables': path.resolve(__dirname, './src/composables'),
+        '@utils': path.resolve(__dirname, './src/composables/utils'), // ✅ FIXED
+      },
+    },
     define: {
       __APP_ENV__: env.APP_ENV,
     },
@@ -41,7 +41,7 @@ export default defineConfig(({ mode }) => {
         output: {
           manualChunks: {
             vue: ['vue', 'vue-router'],
-            utils: ['@utils'],
+            //utils: ['@utils'],
           },
         },
       },
